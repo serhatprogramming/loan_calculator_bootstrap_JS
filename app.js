b02237c08af88e1550b892e4c941ddf6f9b2cf74
serhatprogramming <serhatprogramming@gmail.com>
2022-08-09 22:49:40 -0400

    Initial UI is done

---
+++ app.js
@@ -0,0 +1,10 @@
+// submit listener
+const form = document.getElementById("loan-form");
+form.addEventListener("submit", calculateResults);
+
+// Calculate Results
+function calculateResults(e) {
+  // Form Variables
+  const amount = document;
+  e.preventDefault();
+}
